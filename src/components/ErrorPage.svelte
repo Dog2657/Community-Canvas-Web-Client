bd548273f7bc8c4a5c56ed958c942755d2925bb3
Dog2657 <reeceya2006@gmail.com>
2023-08-17 18:53:31 +0100

    Added Basic loading & error window

---
+++ src/components/ErrorPage.svelte
@@ -0,0 +1,12 @@
+<div class="fail window">
+    
+    Failed to load
+
+</div>
+
+<style lang="scss">
+    div.fail{
+        color: white;
+        background-color: rgba(245, 245, 245, 0.04);
+    }
+</style>
---
+++ src/components/LoadingPage.svelte
@@ -0,0 +1,12 @@
+<div class="loading window">
+    
+    Loading
+
+</div>
+
+<style lang="scss">
+    div.loading{
+        color: white;
+        background-color: rgba(245, 245, 245, 0.04);
+    }
+</style>
