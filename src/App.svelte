33e5135c9823822f8f8daaa41cf96f0a20a432f4
Dog2657 <reeceya2006@gmail.com>
2023-08-17 21:20:49 +0100

    add ts-ignore

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import ErrorPage from './components/ErrorPage.svelte';
   import LoadingPage from './components/LoadingPage.svelte';
   import MainPage from './components/MainPage.svelte';
+  // @ts-ignore
   import { config, notifier, sourceCanvas } from './lib/data'
   
   const loadingPromises = [config, notifier, sourceCanvas]
